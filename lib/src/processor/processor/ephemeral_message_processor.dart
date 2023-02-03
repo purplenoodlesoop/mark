dd42a5a4fa139fde96c8bd52076821a5aa9b6656
Yakov Karpov <yakovkarpoff@gmail.com>
2023-02-04 00:05:46 +0200

    Prepare for release

--- lib/src/processor/processor/ephemeral_message_processor.dart
+++ lib/src/processor/processor/ephemeral_message_processor.dart
@@ -1,9 +1,9 @@
 import 'package:mark/src/message/log_message.dart';
+import 'package:mark/src/processor/formatter/string_message_formatter_mixin.dart';
 import 'package:mark/src/processor/processor/message_processor.dart';
 import 'package:mark/src/processor/processor/print_message_printer.dart'
     if (dart.library.html) 'package:mark/src/processor/processor/web_message_printer.dart'
     if (dart.library.io) 'package:mark/src/processor/processor/io_message_printer.dart';
-import 'package:mark/src/processor/formatter/string_message_formatter_mixin.dart';
 
 /// A [MessageProcessor] that prints the formatted message to the console, using
 /// platform-specific methods.
